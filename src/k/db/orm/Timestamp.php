bab73f1d58bca98ba6502cad1e0571eba65fa935
Thomas <thomas@lekoala.be>
2014-12-29 09:33:47 +0100

    created at

--- src/k/db/orm/Timestamp.php
+++ src/k/db/orm/Timestamp.php
@@ -14,6 +14,10 @@ trait Timestamp {
 
 	public function onPreSaveTimestamp() {
 		if ($this->exists()) {
+			//ensure that created_at exists
+			if(empty($this->created_at)) {
+				$this->saveData['created_at'] = date('Y-m-d H:i:s');
+			}
 			$this->saveData['updated_at'] = date('Y-m-d H:i:s');
 		} else {
 			$this->saveData['created_at'] = date('Y-m-d H:i:s');
